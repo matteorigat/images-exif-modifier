7548ddc27d5661fb2c2cad460f531d33d562f2f9
Matteo Rigat <matteoluigi.rigat@mail.polimi.it>
2024-02-21 22:56:42 +0100

    everything works fine :)

--- main.py
+++ main.py
@@ -7,22 +7,19 @@ import certifi
 import geopy.geocoders
 import requests
 from exif import Image as ExifImage
-
 import piexif
 from PIL import Image
-import numpy as np
-from datetime import datetime
 
 # assign directory
 directory = '/Users/matteorigat/Desktop/images project/test'
 
 # insert the address to set coordinates it can be an address, a city etc
-address = "bormio"  # String or False
+address = "berlino"  # String or False
 
 # new date  -> if False the old values will remain
-new_year = 2024  # like 2023 or False
-new_month = 2    # max 12 or False
-new_day = 10     # max 31 or False
+new_year = 2020  # like 2023 or False
+new_month = 1    # max 12 or False
+new_day = 22     # max 31 or False
 
 # new time -> if False the old values will remain
 new_hour = False     # max 23 or False
@@ -41,31 +38,22 @@ DO NOT EDIT BELOW !!!
 
 """
 
-EXIF_TAGS = [
-    "datetime_original",
-    "gps_latitude",
-    "gps_latitude_ref",
-    "gps_longitude",
-    "gps_longitude_ref",
-    "gps_altitude",
-]
 
-EXIF_ALL_TAGS = ['_exif_ifd_pointer', '_gps_ifd_pointer', 'aperture_value', 'brightness_value', 'color_space',
-                 'components_configuration', 'compression', 'datetime', 'datetime_digitized', 'datetime_original',
-                 'exif_version',
-                 'exposure_bias_value', 'exposure_mode', 'exposure_program', 'exposure_time', 'f_number', 'flash',
-                 'flashpix_version', 'focal_length', 'focal_length_in_35mm_film', 'gps_altitude', 'gps_altitude_ref',
-                 'gps_datestamp', 'gps_dest_bearing', 'gps_dest_bearing_ref', 'gps_horizontal_positioning_error',
-                 'gps_img_direction', 'gps_img_direction_ref', 'gps_latitude', 'gps_latitude_ref', 'gps_longitude',
-                 'gps_longitude_ref', 'gps_speed', 'gps_speed_ref', 'gps_timestamp', 'jpeg_interchange_format',
-                 'jpeg_interchange_format_length', 'lens_make', 'lens_model', 'lens_specification', 'make',
-                 'maker_note',
-                 'metering_mode', 'model', 'orientation', 'photographic_sensitivity', 'pixel_x_dimension',
-                 'pixel_y_dimension',
-                 'resolution_unit', 'scene_capture_type', 'scene_type', 'sensing_method', 'shutter_speed_value',
-                 'software',
-                 'subject_area', 'subsec_time_digitized', 'subsec_time_original', 'white_balance', 'x_resolution',
-                 'y_and_c_positioning', 'y_resolution']
+TAG = [
+    'Make',
+    'Model',
+    'DateTime',
+    'DateTimeOriginal',
+    'DateTimeDigitized',
+    'GPSVersionID',
+    'GPSLatitude',
+    'GPSLatitudeRef',
+    'GPSLongitude',
+    'GPSLongitudeRef',
+    'GPSAltitude',
+    'GPSAltitudeRef',
+    'GPSDateStamp'
+]
 
 # Iterate over files in the directory to select only photos
 def read_path(images):
@@ -121,26 +109,34 @@ def convert_coord(x, check):
     else:
         direction = "E" if degrees >= 0 else "W"
 
-    formatted = (float(abs(degrees)), float(minutes), round(seconds, 2))
+    accuracy = 100  # integer greater than 1, it is a denominator
+    formatted = [(abs(degrees), 1), (minutes, 1), (int(seconds*accuracy), accuracy)]
 
     return formatted, direction
 
 
 # Change exif tags
-def change_tags(images, lat, lon, alt, dateflag, geoflag):
+def change_date(images):
     increment2 = 0
     changed = False
     for img in images:
         image_path = os.path.join(directory, img)
-        with open(image_path, "rb") as input_file:
-            exif_img = ExifImage(input_file)
+        new_img = Image.open(image_path)
 
-        if dateflag and new_year and new_month and new_day:
+        try:
+            exif_dict = piexif.load(new_img.info['exif'])
+        except KeyError:
+            exif_dict = {'0th': {}, 'Exif': {}, 'GPS': {}, '1st': {}, 'thumbnail': None}
+
+        if new_year and new_month and new_day:
             old_date = datetime(1970, 1, 1, 0, 0, 0)
             try:
-                old_date = datetime.strptime(exif_img.get("datetime_original"), "%Y:%m:%d %H:%M:%S")
+                old_date = datetime.strptime(exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal].decode(), "%Y:%m:%d %H:%M:%S")
             except:
                 print("date in a wrong format")
+                if input("Do you want to continue? y/n:\n") != "y":
+                    sys.exit()
+
 
             if new_year: old_date = old_date.replace(year=new_year)
             if new_month: old_date = old_date.replace(month=new_month)
@@ -154,79 +150,80 @@ def change_tags(images, lat, lon, alt, dateflag, geoflag):
                 old_date = old_date + timedelta(seconds=increment2)
                 increment2 += increment
 
-            # old_date = datetime(2020, 1, 1, 0, 0, 0)
-            exif_img.datetime = old_date.strftime("%Y:%m:%d %H:%M:%S")
-            exif_img.datetime_digitized = old_date.strftime("%Y:%m:%d %H:%M:%S")
-            exif_img.datetime_original = old_date.strftime("%Y:%m:%d %H:%M:%S")
+            exif_dict['0th'][piexif.ImageIFD.DateTime] = old_date.strftime("%Y:%m:%d %H:%M:%S")
+            exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = old_date.strftime("%Y:%m:%d %H:%M:%S")
+            exif_dict['Exif'][piexif.ExifIFD.DateTimeDigitized] = old_date.strftime("%Y:%m:%d %H:%M:%S")
+            exif_dict['GPS'][piexif.GPSIFD.GPSDateStamp] = old_date.strftime("%Y:%m:%d")
             changed = True
 
-        if geoflag and address:
-            try:
-                exif_img.gps_latitude, exif_img.gps_latitude_ref = convert_coord(lat, True)
-                exif_img.gps_longitude, exif_img.gps_longitude_ref = convert_coord(lon, False)
-                exif_img.gps_altitude = alt
-                changed = True
-            except:
-                try:
-                    exif_img.gps_latitude = lat
-                    exif_img.gps_longitude = lon
-                    exif_img.gps_altitude = alt
-                    changed = True
-                except:
-                    print("GPS coordinates could not be converted")
-
         if changed:
-            with open(image_path, "wb") as ofile:
-                # DANGER ZONE: you are overwriting your photos data
-                ofile.write(exif_img.get_file())
+            # DANGER ZONE: you are overwriting your photos data
+            exif_bytes = piexif.dump(exif_dict)
+            new_img.save(image_path, exif=exif_bytes)
+
+def change_coords(images, lat, lon, alt):
+
+    for img in images:
+        image_path = os.path.join(directory, img)
+        new_img = Image.open(image_path)
+
+        try:
+            exif_dict = piexif.load(new_img.info['exif'])
+        except KeyError:
+            exif_dict = {'0th': {}, 'Exif': {}, 'GPS': {}, '1st': {}, 'thumbnail': None}
+
+        # Set GPS Info
+        exif_dict['GPS'][piexif.GPSIFD.GPSVersionID] = (2, 2, 0, 0)
+        exif_dict['GPS'][piexif.GPSIFD.GPSLatitude], exif_dict['GPS'][piexif.GPSIFD.GPSLatitudeRef] = convert_coord(lat, True)
+        exif_dict['GPS'][piexif.GPSIFD.GPSLongitude], exif_dict['GPS'][piexif.GPSIFD.GPSLongitudeRef] = convert_coord(lon, False)
+        exif_dict['GPS'][piexif.GPSIFD.GPSAltitude], exif_dict['GPS'][piexif.GPSIFD.GPSAltitudeRef] = (int(alt*100), 100), 0
+
+        # DANGER ZONE: you are overwriting your photos data
+        exif_bytes = piexif.dump(exif_dict)
+        new_img.save(image_path, exif=exif_bytes)
 
 
-# View main data, used most of the time to get a quick overview of the situation
 def view_data(images):
     for img in images:
-        print(img)
+        print("\033[93m" + img + "\033[0m")
         image_path = os.path.join(directory, img)
-        with open(image_path, "rb") as input_file:
-            img = ExifImage(input_file)
+        new_img = Image.open(image_path)
+        try:
+            exif_dict = piexif.load(new_img.info['exif'])
+        except Exception as e:
+            print(e)
 
-        for tag in EXIF_TAGS:
-            try:
-                value = img.get(tag)
-                print("{}: {}".format(tag, value))
-            except:
-                print("Tag {} could not be read".format(tag))
-                sys.exit()
-
-        if img.has_exif:
-            status = f"contains EXIF (version {img.exif_version}) information."
-        else:
-            status = "does not contain any EXIF information."
-        print(f"Image {status}")
+        for ifd in ("0th", "Exif", "GPS", "1st"):
+            for tag in exif_dict[ifd]:
+                if piexif.TAGS[ifd][tag]["name"] in TAG:
+                    print(piexif.TAGS[ifd][tag]["name"], exif_dict[ifd][tag])
         print("")
 
-
-# View all exif data, since this print many lines it has as input a single image
-def view_first_all_data(img):
-    print(img)
-    image_path = os.path.join(directory, img)
-    with open(image_path, "rb") as input_file:
-        img = ExifImage(input_file)
-
-    for tag in EXIF_ALL_TAGS:
-        value = img.get(tag)
-        if (value is not None):
-            print("{}: {}".format(tag, value))
-
-
-# View only the date of all the images in the directory
-def view_all_dates(images):
+def print2(images):
     for img in images:
-        print(img)
         image_path = os.path.join(directory, img)
-        with open(image_path, "rb") as input_file:
-            img = ExifImage(input_file)
+        exif_dict = piexif.load(image_path)
 
-        print("{}: {}".format("datetime_original", img.get("datetime_original")))
+        # Extract thumbnail and save it, if exists
+        thumbnail = exif_dict.pop('thumbnail')
+        if thumbnail is not None:
+            with open('thumbnail.jpg', 'wb') as f:
+                f.write(thumbnail)
+
+        # Iterate through all the other ifd names and print them
+        print(f'Metadata for {img}:')
+        for ifd in exif_dict:
+            print(f'{ifd}:')
+            for tag in exif_dict[ifd]:
+                tag_name = piexif.TAGS[ifd][tag]["name"]
+                tag_value = exif_dict[ifd][tag]
+                # Avoid print a large value, just to be pretty
+                if isinstance(tag_value, bytes):
+                    tag_value = tag_value[:10]
+                print(f'\t{tag_name:25}: {tag_value}')
+        print()
+
+
 
 
 if __name__ == '__main__':
@@ -236,12 +233,9 @@ if __name__ == '__main__':
     read_path(images)
 
     # view data
-    view_data(images)                           # View main data, used most of the time to get a quick overview of the situation
-    # view_all_dates(images)                    # View all exif data, since this print many lines it has as input a single
-    # image view_first_all_data(images[0])      # View only the date of all the images in the directory
+    view_data(images)
 
     # Retrieve coordinates
-    lat, lon, alt = 0, 0, 0
     if address:
         print("\n")
         address, lat, lon, alt = find_coordinateds()
@@ -254,7 +248,11 @@ if __name__ == '__main__':
     geoflag = input("\nyou are changing the location to \033[93m" + str(address) + "\033[0m are you sure? y/n:\n") == "y"
 
 
-    if dateflag or geoflag:
-        change_tags(images, lat, lon, alt, dateflag, geoflag)
+    if dateflag:
+        change_date(images)
+    if geoflag and address:
+        change_coords(images, lat, lon, alt)
+
+    if (dateflag or geoflag) and  input("\nprint output? y/n:\n") == "y":
         print("\nAFTER\n")
         view_data(images)
